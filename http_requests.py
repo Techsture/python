0079a5ee3c86df0335082465126ae1e232cc5335
Michael <michael@Michaels-Mac-mini.local>
2017-04-10 21:16:42 -0700

    http_requests.py

---
+++ http_requests.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python
+
+import requests
+
+
+def main():
+    r = requests.get("http://www.apple.com")
+    print(r.status_code)
+    print(r.headers)
+    print(r.content)
+
+
+if __name__ == "__main__":
+    main()
