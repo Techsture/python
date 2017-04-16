f9431366dbe7219b13898df2bb415096a57f9ed5
Michael <michael@Michaels-Mac-mini.local>
2017-04-15 23:28:25 -0700

    palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,22 @@
+#!/usr/local/bin/python
+
+# Test if a given string is a palindrome.
+
+import argparse
+
+def main():
+    # Parse the argument:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("parameter", help="string to test", type=str)
+    args = parser.parse_args()
+    # Actual program starts here:
+    parsed_string = args.parameter.lower()
+    reversed_string = parsed_string[::-1]
+    if parsed_string == reversed_string:
+        print("%s is a palindrome!" % args.parameter)
+    else:
+        print("%s is not a palindrome." % args.parameter)
+
+
+if __name__ == "__main__":
+    main()
