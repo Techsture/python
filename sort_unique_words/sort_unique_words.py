1f5668d3a99ebb5290042c427b7d68f1223dad36
Michael <michael@Michaels-Mac-mini.local>
2017-04-16 11:10:38 -0700

    sort_unique_words and baby_names (pre-solution)

---
+++ sort_unique_words/sort_unique_words.py
@@ -0,0 +1,29 @@
+#!/usr/local/bin/python
+
+# Given a file, sort all the words in that file.
+# Print out all the words in a list.
+# If a word already exists in the list, don't add it again.
+
+# Need to import 'string' so punctuation can be stripped easily
+import string
+
+
+def main():
+    # Open the file
+    in_file = open("gibberish.txt", 'r')
+    # Split the file on whitespace so that you get a list of words
+    word_list = in_file.read().split()
+    unique_list = []
+    for item in word_list:
+        # Convert eat item to lowercase and strip off the punctuation
+        lowercase_item = item.lower().translate(None, string.punctuation)
+        # If the item isn't in the list, add it
+        if lowercase_item not in unique_list:
+            unique_list.append(lowercase_item)
+    # Sort the list and print it
+    unique_list.sort()
+    print(unique_list)
+
+
+if __name__ == "__main__":
+    main()
