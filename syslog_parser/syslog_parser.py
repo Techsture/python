0a304ecff87ef4056e47b0bc06e24cb095038ebc
Michael Andrews <mail@michaelandrews.net>
2017-04-16 19:15:54 -0700

    syslog_parser.py initial commit

---
+++ syslog_parser/syslog_parser.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# Small script to search through syslog files.
+# Assumes there's a syslog file in the script directory (./)
+
+
+import re
+
+
+def main():
+    exit()
+
+
+if __name__ == '__main__':
+    main()
