5153bdffc61501fcb51336dc13eb38d8a575c393
Michael Andrews <mail@michaelandrews.net>
2017-04-16 19:54:35 -0700

    recursive_addition

---
+++ recursive_addition.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# Script to recursively add together all off the integers up to and including the integer passed in.
+
+import argparse
+
+
+def recursive_add(integer):
+    if integer == 0:
+        return 0
+    return integer + recursive_add(integer - 1)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("integer", help="interger", type=int)
+    args = parser.parse_args()
+    if args.integer > 997:
+        print("Number is too large!")
+        exit()
+    else:
+        print(recursive_add(args.integer))
+
+
+if __name__ == '__main__':
+    main()
+    
