72a68edb267aab3ebc9e0a092ec154af37728c48
Michael Andrews <mandrews4@apple.com>
2019-09-03 09:59:52 -0700

    moving some stuff around

--- netrunner/core.py
+++
@@ -1,4 +0,0 @@
-core_list = [
-  "Original Core",
-  "Revised Core"
-]
--- netrunner/expansions.py
+++
@@ -1,7 +0,0 @@
-expansion_list = [
-  "Creation and Control",
-  "Honor and Profit",
-  "Order and Chaos",
-  "Data and Destiny",
-  "Reign and Reverie"
-]
